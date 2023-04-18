1397164d1185e4f62d2e6ba74209675d0a5c97a7
Carlos Raez <carlosraez@gmail.com>
2023-04-18 12:30:13 +0200

    feat mongo Database schema

---
+++ models/Product.ts
@@ -0,0 +1,69 @@
+import { IProduct } from "@/interfaces/products";
+import mongoose, { Model, Schema, model } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    description: {
+      type: String,
+      default:
+        "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
+    },
+    images: {
+      type: [String],
+    },
+    inStock: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    sizes: {
+      type: String,
+      enum: {
+        values: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+        message: "{Value} no es un tamaño permitido",
+      },
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    type: {
+      type: String,
+      enum: {
+        values: ["shirts", "pants", "hoodies", "hats"],
+        message: "{Value} no es un tipo permitido",
+      },
+      required: true,
+    },
+    tags: {
+      type: [String],
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["men", "women", "kid", "unisex"],
+        message: "{Value} no es un genero permitido",
+      },
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//TODO: Crear Indice de Mongo
+const Product: Model<IProduct> =
+  mongoose.models.Product || model("Product", productSchema);
+
+export default Product;
---
+++ src/pages/api/seed.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connect, disconnect } from "../../../database/db";
+import Product from "../../../models/Product";
+import { db, seedDatabase } from "../../../database";
+
+type Data = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (process.env.NODE_ENV === "production") {
+    return res.status(401).json({ message: "No tiene acceso a este API" });
+  }
+
+  await db.connect();
+  await Product.deleteMany();
+  await Product.insertMany(seedDatabase.initialData.products);
+  await db.disconnect();
+
+  res.status(200).json({ message: "Proceso realizado correctamente" });
+}
